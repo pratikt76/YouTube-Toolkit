b73710d17995be294f5dd057b62382cfdd0b845c
Pratik Thombare <thombareps20.extc@coep.ac.in>
2022-12-06 18:52:58 +0530

    minor changes

--- bg.js
+++ bg.js
@@ -1,6 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "loading") {
-    if (tab.url === "https://www.youtube.com/") {
+    if (
+      tab.url === "https://www.youtube.com/" ||
+      tab.url != "https://www.youtube.com/feed/subscriptions"
+    ) {
       chrome.tabs.insertCSS(
         tabId,
         {
